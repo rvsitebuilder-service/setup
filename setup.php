2483bb295405df5893ac49cb399333b4ff94b1f8
Arnut Klamhui <arnut@rvglobalsoft.com>
2019-03-04 13:07:23 +0700

    update getheader and logging
    Signed-off-by: Arnut Klamhui <arnut@rvglobalsoft.com>

--- setup.php
+++ setup.php
@@ -7,7 +7,7 @@ use splitbrain\PHPArchive\Tar;
 //get installer config
 $installerconfig = getInstallerConfig();
 
-$headers = apache_request_headers();
+$headers = (function_exists('apache_request_headers') || is_callable('apache_request_headers'))  ? apache_request_headers() : rv_apache_request_headers();
 $installtype  = (isset($headers['Installtype'])) ? $headers['Installtype'] : 'nocp';
 print_debug_log($installerconfig['debug_log'],'Install type '.$installtype);
 
@@ -126,4 +126,31 @@ function print_install_log($installlog , $msg = '') {
     }
     return true;
 }
+
+
+function rv_apache_request_headers() {
+    $arh = array();
+    $rx_http = '/\AHTTP_/';
+    foreach($_SERVER as $key => $val) {
+        if( preg_match($rx_http, $key) ) {
+            $arh_key = preg_replace($rx_http, '', $key);
+            $rx_matches = array();
+            // do some nasty string manipulations to restore the original letter case
+            // this should work in most cases
+            $rx_matches = explode('_', $arh_key);
+            if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+                foreach($rx_matches as $ak_key => $ak_val) {
+                    $rx_matches[$ak_key] = ucfirst($ak_val);
+                    $arh_key = implode('-', $rx_matches);
+                    $arh_key =  ucfirst(strtolower($arh_key));
+                }
+            }
+            $arh[$arh_key] = $val;
+        }
+    }
+    return( $arh );
+}
+
+
+
 ?>
