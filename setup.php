816fdfd9111aa03ad9cf68a9b21df2a39a92bc52
arnut <arnut@rvglobalsoft.com>
2019-09-19 16:33:28 +0700

    install provision mode

--- setup.php
+++ setup.php
@@ -6,10 +6,18 @@ use splitbrain\PHPArchive\Tar;
 
 //get installer config
 $installerconfig = getInstallerConfig();
+//get provision config
+$provisionconfig = getProvisionConfig();
 
+//get header
 $headers = (function_exists('apache_request_headers') || is_callable('apache_request_headers'))  ? apache_request_headers() : rv_apache_request_headers();
 $headers = array_change_key_case($headers,CASE_UPPER);
-$installtype  = (isset($headers['INSTALLTYPE'])) ? $headers['INSTALLTYPE'] : 'nocp';
+
+//set installtype
+$installtype =  'nocp';
+if(isset($headers['INSTALLTYPE']) && isset($headers['INSTALLTYPE']) != '') $installtype = $headers['INSTALLTYPE'];
+if(isset($provisionconfig['provisioning'])) $installtype = 'provision';
+
 print_debug_log($installerconfig['debug_log'],'Install type '.$installtype);
 $rvlicensecode = (isset($headers['RV-LICENSE-CODE'])) ? $headers['RV-LICENSE-CODE'] : '';
 print_debug_log($installerconfig['debug_log'],'RV License Code '.$rvlicensecode);
@@ -25,14 +33,6 @@ if (!extension_loaded('json')) {
 //json header
 header('Content-type: application/json');
 
-//common validation
-/*
-if(ini_get('allow_url_fopen') != 1){
-    echo json_encode( ['status' => false , 'message' => 'Error php.ini, Must set allow_url_fopen=ON'] );
-    print_install_log($installerconfig['install_log'] , 'Error php.ini, Must set allow_url_fopen=ON');
-    exit;
-}
-*/
 
 //if not file setupapiserver or install.html
 if (! file_exists(dirname(__FILE__).'/install.html') || ! file_exists(dirname(__FILE__).'/setupapiserver.php')) {
@@ -69,11 +69,19 @@ if (! file_exists(dirname(__FILE__).'/install.html') || ! file_exists(dirname(__
 }
 
 //Go Go Go
+//nocp
 if($installtype == 'nocp') {
     print_install_log($installerconfig['install_log'] , 'Redirect to real installer INSTALL.HTML');
-    print_debug_log($installerconfig['debug_log'],'Redirect to real installer INSTALL.HTML');
+    print_debug_log($installerconfig['debug_log'],'Redirect to installer INSTALL.HTML');
     header("Location: install.html");
 }
+//provision
+elseif($installtype == 'provision') {
+    print_install_log($installerconfig['install_log'] , 'Redirect to real installer PROVISION.HTML');
+    print_debug_log($installerconfig['debug_log'],'Redirect to installer PROVISION.HTML');
+    header("Location: provision.html");
+}
+//cpanel
 else {
     print_install_log($installerconfig['install_log'] , 'Redirect to real installer SETUPAPISERVER.PHP');
     print_debug_log($installerconfig['debug_log'],'Redirect to real installer SETUPAPISERVER.PHP');
@@ -109,6 +117,128 @@ function getInstallerConfig() {
     $installerconfig2 = array_merge($installerconfig1,$userconfig);
     
     return $installerconfig2;
+}
+
+function getProvisionConfig(){
+    $provisionConfig = [];
+    $userpathinfo = get_user_path_info();
+    $domainname = get_current_domain();
+    $configfile = $userpathinfo['homepath'] . '/rvsitebuildercms/' . $domainname . '/provisioning.ini';
+    if(file_exists($configfile)) {
+        $provisionConfig = parse_ini_file($configfile,true);
+    }
+    
+    if(!isset($provisionConfig['provisioning']['db_host']) || !isset($provisionConfig['provisioning']['db_name']) || 
+       !isset($provisionConfig['provisioning']['db_user']) || !isset($provisionConfig['provisioning']['db_pass']) ||
+       !isset($provisionConfig['provisioning']['admin_email']) || !isset($provisionConfig['provisioning']['admin_password'])
+      ){
+          return [];
+    }
+    
+    return $provisionConfig;
+}
+
+function get_current_domain() {
+    $domainname = '';
+    if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] != '') {
+        $domainname = $_SERVER['HTTP_HOST'];
+    }
+    if(isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] != ''){
+        $domainname = $_SERVER['SERVER_NAME'];
+    }
+    $domainname = str_replace("www.","",$domainname);
+    return $domainname;
+}
+
+function get_user_path_info(){
+    $userpathinfo = [];
+    $user_path = '';
+    $document_root = $_SERVER['DOCUMENT_ROOT'];
+    $user_paths = [];
+    $open_basedir_paths = get_open_basedir_paths();
+    if(count($open_basedir_paths)) {
+        $user_paths = $open_basedir_paths;
+    } else {
+        if(isset($_SERVER['HOME'])) {
+            if (!in_array($_SERVER['HOME'], $user_paths)) {
+                array_push($user_paths, $_SERVER['HOME']);
+            }
+        }
+        $posix_user_path = '';
+        if(function_exists('posix_getuid')) {
+            $webuid = posix_getuid();
+            $userinfo = posix_getpwuid($webuid);
+            if(is_dir($userinfo['dir'])){
+                if (!in_array($userinfo['dir'], $user_paths)) {
+                    array_push($user_paths, $userinfo['dir']);
+                }
+            }
+        }
+        // case  have posix_getpwuid get uid by owner dir
+        if(function_exists('posix_getpwuid')) {
+            $stat = stat($document_root);
+            $userinfo = posix_getpwuid($stat['uid']);
+            if(is_dir($userinfo['dir'])) {
+                if (!in_array($userinfo['dir'], $user_paths)) {
+                    array_push($user_paths, $userinfo['dir']);
+                }
+            }
+            $userinfo = posix_getpwuid($stat['gid']);
+            if(is_dir($userinfo['dir'])) {
+                if (!in_array($userinfo['dir'], $user_paths)) {
+                    array_push($user_paths, $userinfo['dir']);
+                }
+            }
+        }
+        // case  find home path from document_root ( /home/amarin/public_html => /home/amarin )
+        if($posix_user_path == '') {
+            $paths = preg_split("/\//", $document_root);
+            $loop_dim = count($paths);
+            for($i=0; $i < $loop_dim; $i++) {
+                $test_path = join('/', $paths);
+                if(is_dir($test_path)) {
+                    if (!in_array($test_path, $user_paths)) {
+                        array_push($user_paths, $test_path);
+                    }
+                }
+                array_pop($paths);
+            }
+        }
+    }
+    
+    foreach($user_paths as $user_path_var) {
+        if(is_file($user_path_var)) {
+            continue;
+        }
+        if($user_path_var == $document_root) {
+            continue;
+        }
+        if(preg_match("/(^\/tmp)|(\/tmp$)/",$user_path_var)){
+            continue;
+        }
+        if(is_writable($user_path_var)) {
+            $user_path = $user_path_var;
+            break;
+        }
+    }
+    
+    $userpathinfo['homepath'] = $user_path;
+    $userpathinfo['publicpath'] = $document_root;
+    
+    return $userpathinfo;
+}
+
+function get_open_basedir_paths() {
+    $open_basedir_paths = [];
+    if(function_exists('ini_get')){
+        $open_basedir_str =  ini_get('open_basedir');
+        $open_basedir_str = trim($open_basedir_str);
+        if($open_basedir_str != ''){
+            $open_basedir_str = strtolower($open_basedir_str);
+            $open_basedir_paths = explode(":", $open_basedir_str);
+        }
+    }
+    return $open_basedir_paths;
 }
 
 function doDownload($type, $url, $sink, $rvlicensecode,$debug_log , $getversionurl) {
@@ -174,14 +304,6 @@ function doDownload($type, $url, $sink, $rvlicensecode,$debug_log , $getversionu
     }
     
     return $response;
-}
-
-function get_current_domain() {
-    $domainname = $_SERVER['SERVER_NAME'];
-    if (preg_match('/^www\./', $domainname)) {
-        $domainname = str_replace("www.","",$domainname);
-    }
-    return $domainname;
 }
 
 function get_client_ip() {
